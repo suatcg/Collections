e83c724bb621a0304a1e3c51f6d177f3c350431f
suatcg <suat-can_24@hotmail.com>
2020-11-15 14:41:23 +0300

    initial commit

---
+++ Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CollectionsArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wensday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+            daysOfWeek[2] = "Wednesday";
+
+            Console.WriteLine("which day do you want to display?");
+            Console.Write("(Monday = 1 etc.) > ");
+            int iDay = int.Parse(Console.ReadLine());
+
+            string chosenDay = daysOfWeek[iDay-1];
+            Console.WriteLine($"That day is {chosenDay}");
+
+            foreach (var day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            
+        }
+    }
+}
---
+++ TopTenPops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Suat-\PopByLargestFinal.csv";
+
+            var reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine($"{PopulationFormat.FormatPopulation(country.Population).PadLeft(15) }: {country.Name}");
+            }
+        }
+    }
+}
