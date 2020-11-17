148494f7eed4399c1069f3a257e8f968401b05e6
suatcg <suat-can_24@hotmail.com>
2020-11-17 21:16:19 +0300

    git pull origin master

---
+++ CollectionsDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using ReadAllCountries;
+using System;
+using System.Collections.Generic;
+
+namespace CollectionsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var norway = new Country("Norway", "NOR", "Europe", 5_282_223);
+            var finland = new Country("Findland", "FIN", "Europe", 5_511_303);
+
+            var countries = new Dictionary<string, Country>
+            {
+                { norway.Code, norway },
+                { finland.Code, finland }
+            };
+
+            // Other way
+            //countries.Add(norway.Code, norway);
+            //countries.Add(finland.Code, finland);
+
+            foreach (var country in countries.Values)
+            {
+                Console.WriteLine(country.Name);
+            }
+        }
+    }
+}
--- TopTenPops/Program.cs
+++ TopTenPops/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+
 
 namespace ReadAllCountries
 {
@@ -14,11 +16,46 @@ namespace ReadAllCountries
 
             List<Country> countries = reader.ReadAllCountries();
 
-            //countries.OrderByDescending(x => x.Population);
+
+
+            var liliput = new Country("Liliput", "LIL", "Somewhere", 2_000_000);
+            List<Country> liliputStartWithL = countries.FindAll(x => x.Name.StartsWith('L'));
+
+            var valueChar = GetValueLili(liliputStartWithL);
+            
+            var liliputIndexOne = liliputStartWithL.FindIndex(x => x.Name.ElementAt(1).Equals((char)(valueChar)));
+
+            var firstLIndex = countries.FindIndex(x => x.Name.StartsWith('L'));
+
+            countries.Insert(firstLIndex+ liliputIndexOne, liliput);
+
+
+            static byte GetValueLili(List<Country> list)
+            {
+                var newlist = new List<byte>();
+                foreach (var lili in list)
+                {
+                    // 97 = 'a' ASCII code ,105 = 'i' ASCII code
+                    if ((byte)lili.Name[1] >= 97 & (byte)lili.Name[1] <= 105)
+                    {
+                        newlist.Add((byte)lili.Name[1]);
+                    }
+                }
+
+                return (byte)newlist.Max();
+                
+            }
+            
+            
+            
+
+
             foreach (var country in countries)
             {
                 Console.WriteLine($"{PopulationFormat.FormatPopulation(country.Population).PadLeft(15) }: {country.Name}");
             }
+
+            Console.WriteLine($"{countries.Count} countries");
         }
     }
 }
